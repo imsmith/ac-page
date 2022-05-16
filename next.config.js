ff8dbd9c7b888196a50dcfa307a3bc33270c9f4a
Dallas Norton <d.norton@f5.com>
2022-05-16 15:16:22 -0600

    removing extra deps

--- next.config.js
+++ next.config.js
@@ -1,23 +1,13 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: []
-    // If you use `MDXProvider`, uncomment the following line.
-    // providerImportSource: '@mdx-js/react',
-  }
-});
-
-module.exports = withMDX({
-  // Append the default value with md extensions
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+const nextConfig = {
   reactStrictMode: true,
   images: {
     loader: 'custom',
     domains: ['unsplash.com', 'images.unsplash.com', 'f5.sharepoint.com']
   },
   assetPrefix: ''
-});
+};
+
+module.exports = nextConfig;
